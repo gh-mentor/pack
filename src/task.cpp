aa30a8ca77d9a3744aac5b6d2e2a283bb6cdd4b0
Andrew Scoppa <andrew-scoppa@github.com>
2025-01-09 11:50:21 -0500

    updated the code to resolve the issue

--- src/task.cpp
+++ src/task.cpp
@@ -3,20 +3,17 @@
 #include <future>
 #include <iostream>
 
-/*
-This function executes a given task on a separate thread.
-Arguments:
-    t: a function that takes an integer as an argument and returns void.
-    load: an integer that is passed to the function t which is used to simulate a workload.
-Returns:
-    void 
-Exceptions:
-    The function will throw an exception if the task t throws an exception.
-*/
+/**
+ * This function executes a given task on a separate thread.
+ * @param t a function that takes an integer as an argument and returns void.
+ * @param load an integer that is passed to the function t which is used to simulate a workload.
+ * @return void
+ * @throws std::exception if the task t throws an exception.
+ */
 void execute(std::function<void(int)> t, int load) {
     std::packaged_task<void(int)> task(t);
     std::future<void> future = task.get_future();
-    std::thread worker(task, load);
+    std::thread worker(std::move(task), load);
     worker.join();
     try {
         future.get();
